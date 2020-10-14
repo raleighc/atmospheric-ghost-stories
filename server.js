d58f9de10d1a9723de951cde2439bd43c0f7aabc
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-13 23:20:58 -0400

    added get route to server to check the connection of handlebars. Created a js folder and script file in public to finish link connection to main.handlebars.

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
-// *** Dependencies
-// =============================================================
+// Requiring Dependencies
 const express = require("express");
 const exphbs = require("express-handlebars");
+
 // Sets up the Express App
-// =============================================================
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Requiring our models for syncing
-// =============================================================
 // var db = require("./models");
 
 // Express Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Static directory
 app.use(express.static("public"));
 
 // Handlebars Middleware
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
-// Routes
-// =============================================================
+// API Routes
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
   });
 });
 
+// Views Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 // Syncing our sequelize models and then starting our Express app
-// =============================================================
 // db.sequelize.sync().then(function() {
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
