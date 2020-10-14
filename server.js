998ba982ab1115a1299cd2984a3e874838ca4d27
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-13 22:39:26 -0400

    added server api route for testing connection. requires express-handlebars and added handlebars middleware.

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
 // *** Dependencies
 // =============================================================
-var express = require("express");
-
+const express = require("express");
+const exphbs = require("express-handlebars");
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Requiring our models for syncing
 // =============================================================
 // var db = require("./models");
 
-// Sets up the Express app to handle data parsing
+// Express Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // Static directory
 app.use(express.static("public"));
 
+// Handlebars Middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
 // Routes
 // =============================================================
-
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 // db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
 // });
