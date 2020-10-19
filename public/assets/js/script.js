1fddf40e33efbcf27714af7899eff63741b68f49
Lauren Noeltner <laurennoeltner@gmail.com>
2020-10-18 20:32:54 -0400

    added comments to all rooms and script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,122 +1,100 @@
 $(document).ready(function () {
+  //on click 'gogoGhostie', initialize interaction
   $("#gogoGhostie").on("click", function (event) {
     event.preventDefault();
     console.log("this clicked");
     $("#gogoGhostie").addClass("hide");
     $("#gogoGhostie").removeClass("show");
-    // Hello USERNAME
     $("#interaction1-0").removeClass("hide");
   });
-  // first response option --LVL 1-1
+  // first response option --LVL 1
+  //if positive, show 1-1
   $("#positiveBtn").on("click", function (event) {
     event.preventDefault();
-    // Hello USERNAME
     $("#interaction1-0").addClass("hide");
-    ////my name is GERTRUDE
     $("#interaction1-1").removeClass("hide");
   });
+  //if negative, show 1-2
   $("#negativeBtn").on("click", function (event) {
     event.preventDefault();
-    // Hello USERNAME
     $("#interaction1-0").addClass("hide");
-    // Oh, calm down
     $("#interaction1-2").removeClass("hide");
   });
-  // second response option
-  // p=1-3
+  // LVL 2
+  // if positive, show 1-3
   $("#positiveBtn2").on("click", function (event) {
     event.preventDefault();
-    // my name is GERTRUDE
     $("#interaction1-1").addClass("hide");
-    // Oh, calm down
     $("#interaction1-2").addClass("hide");
-    //I was born in ST LOUIS
     $("#interaction1-3").removeClass("hide");
   });
+  //if negative, show 1-4
   $("#negativeBtn2").on("click", function (event) {
     event.preventDefault();
     $("#interaction1-0").addClass("hide");
-    // my name is GERTRUDE
     $("#interaction1-1").addClass("hide");
-    // Oh, calm down
     $("#interaction1-2").addClass("hide");
-    //I was born in ST LOUIS
     $("#interaction1-4").removeClass("hide");
   });
-
+  //LVL 3
+  //if positive, show 1-3
   $("#positiveBtn3").on("click", function (event) {
     event.preventDefault();
     $("#interaction1-0").addClass("hide");
-    //stlouis
     $("#interaction1-1").addClass("hide");
-    // Oh, calm down
     $("#interaction1-2").addClass("hide");
-    //This is my home.
     $("#interaction1-3").removeClass("hide");
   });
-  // neg=>1-4
+  //if negative, show 1-4
   $("#negativeBtn3").on("click", function (event) {
     event.preventDefault();
     $("#interaction1-0").addClass("hide");
-    //stlouis
     $("#interaction1-1").addClass("hide");
-    // Oh, calm down
     $("#interaction1-2").addClass("hide");
-    //This is my home.
     $("#interaction1-4").removeClass("hide");
   });
-
+  //LVL 4
+  //if positive, show 1-5
   $("#positiveBtn4").on("click", function (event) {
     event.preventDefault();
     console.log("you clicked");
-    //I was born in ST LOUIS
     $("#interaction1-0").addClass("hide");
     $("#interaction1-1").addClass("hide");
     $("#interaction1-2").addClass("hide");
     $("#interaction1-3").addClass("hide");
-    //This is my Home
     $("#interaction1-4").addClass("hide");
-    //There are other spirits like myself
     $("#interaction1-5").removeClass("hide");
-    //   //show btns 6
   });
-  // neg=>1-6
+  // if neg, show 1-6
   $("#negativeBtn4").on("click", function (event) {
     event.preventDefault();
     console.log("you clicked");
     $("#interaction1-0").addClass("hide");
     $("#interaction1-1").addClass("hide");
     $("#interaction1-2").addClass("hide");
-    //I was born in ST LOUIS
     $("#interaction1-3").addClass("hide");
-    //This is my Home
     $("#interaction1-4").addClass("hide");
-    //GET OUT
     $("#interaction1-6").removeClass("hide");
   });
+  //LVL 5
+  //if positive, show 1-5
   $("#positiveBtn5").on("click", function (event) {
     event.preventDefault();
     $("#interaction1-0").addClass("hide");
-    // my name is GERTRUDE
     $("#interaction1-1").addClass("hide");
-    // Oh, calm down
     $("#interaction1-2").addClass("hide");
     $("#interaction1-3").addClass("hide");
     $("#interaction1-4").addClass("hide");
-    //I was born in ST LOUIS
     $("#interaction1-5").removeClass("hide");
   });
-  // // neg=>1-6
+  //if negative, show 1-6
   $("#negativeBtn5").on("click", function (event) {
     event.preventDefault();
     $("#interaction1-0").addClass("hide");
-    //my name is GERTRUdE
     $("#interaction1-1").addClass("hide");
     $("#interaction1-2").addClass("hide");
     $("#interaction1-3").addClass("hide");
-    // Oh, calm down
     $("#interaction1-4").addClass("hide");
-    //get out
     $("#interaction1-6").removeClass("hide");
   });
 });
